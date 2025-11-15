fe8e602553a1ffffe8fc6c9b40f04d349ae9d0eb
Tesfamichael Abebe <tesfamichael.abebe-ug@aau.edu.et>
2025-11-15 22:21:27 +0300

    Time: 2 ms (56.42%), Space: 17.7 MB (76.93%) - LeetHub

---
+++ 1570-final-prices-with-a-special-discount-in-a-shop/1570-final-prices-with-a-special-discount-in-a-shop.py
@@ -0,0 +1,13 @@
+class Solution:
+    def finalPrices(self, prices: List[int]) -> List[int]:
+        stack = []
+        res = prices[:]  
+
+        for i in range(len(prices)):
+            while stack and prices[i] <= prices[stack[-1]]:
+                idx = stack.pop()
+                res[idx] = prices[idx] - prices[i]
+            stack.append(i)
+
+        return res
+        
